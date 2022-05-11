bfa98d000200d961336ab012393a1bb3366d9751
David Lazo Vasquez <lazovasquez@usp.br>
2022-05-11 12:09:22 -0300

    update

---
+++ diff_maps.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import json
+import argparse
+import numpy as np
+import h5py
+# from mayavi import mlab
+import matplotlib.pyplot as plt
+
+# extra
+import os
+import warnings
+import subprocess
+from json_extract import GetValue2
+
+# warnings.filterwarnings('ignore')
